72ea963e426994191b460db970bdc5374b47fcdc
Kealoha1988 <kealohaabe1988@gmail.com>
2020-11-30 17:31:49 -0500

    commit tree de69d452d95d1d507ab40ebe9ab4856c28c64373

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
 # Write your code here
+
+require 'rest-client'
+require 'json'
+
+
+class GetRequester
+    attr_accessor :URL
+
+    def initialize(url)
+        @URL = url
+
+    end
+    
+    def get_response_body
+        RestClient.get(URL)
+
+    end
+
+
+    def parse_json
+        JSON.parse(get_response_body)
+  
+    end
+
+end
